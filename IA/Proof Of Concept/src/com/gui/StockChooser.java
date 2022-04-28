996a3d398d7872f81485c763a9ac66f87fd8ce8b
16koirala_l <16koirala_l@bexleygs.co.uk#>
2022-04-28 10:15:18 +0100

    Added StockPicker GUI

--- IA/Proof Of Concept/src/com/gui/StockChooser.java
+++ IA/Proof Of Concept/src/com/gui/StockChooser.java
@@ -1,16 +1,20 @@
+package com.gui;
+
+import com.stock.Stock;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class GUI extends JPanel {
+public class StockChooser extends JPanel {
     // canvas for other GUI widgets
 
     JButton button;
     JTextField textfield;
+    JLabel label;
 
-    public GUI(int width, int height) {
+    public StockChooser(int width, int height) {
         System.out.println("SEQUENCE: GUI constructor");
         this.setPreferredSize(new Dimension(width, height));
         setLayout(null);
@@ -28,8 +32,19 @@ public class GUI extends JPanel {
             public void actionPerformed(ActionEvent ae){
                 String textFieldValue = textfield.getText();
                 System.out.println(textFieldValue);
+
+                try {
+                    Stock stock = new Stock(textFieldValue);
+                    String historical_data = stock.getHistorical_data();
+
+                    label = new JLabel(historical_data);
+                    label.setBounds(0,50, 100, 40);
+
+
+                } catch (Exception e) {
+                    System.out.println("Stock Doesn't Exists"); // TODO: check if it's true if not print the error message
+                }
             }
         });
-
     }
 }
